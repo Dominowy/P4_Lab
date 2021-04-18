82252d2a49444dd6429bec53c406a0479ba4faff
Dominik Katana <hejdominowy@outlook.com>
2021-04-18 19:52:52 +0200

    Final

---
+++ Lab.2/Lab.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lab._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DataBase(ConnStr.conn());
+            
+            // db.AddEmployees(new Employees() {FirstName = "Maciej", LastName = "Katana"});
+            //
+            // db.DropEmployees(10);
+            //
+            // db.UpdateEmpoyee(10, "Maciej");
+            //
+            // Console.WriteLine();
+            
+            foreach (Employees employees in db.GetEmployees())
+            {
+                Console.WriteLine($"{employees.EmployeeID}: {employees.LastName}, {employees.FirstName}");
+            }
+            
+            Console.WriteLine();
+            
+            var singleEmployees = db.GetEmployeesByID(10);
+            Console.WriteLine($"{singleEmployees.EmployeeID}: {singleEmployees.LastName}, {singleEmployees.FirstName}");
+        }
+    }
+}
