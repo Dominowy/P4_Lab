13aab65b37d3f952b3c9f1f99c36056200257273
Dominik Katana <hejdominowy@outlook.com>
2021-04-18 19:52:03 +0200

    Final

---
+++ Lab.2/Lab.2/ConnStr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lab._2
+{
+    public class ConnStr
+    {
+        public static string conn()
+        {
+            var connect = @"Data Source = ServerAddress; Initial Catalog = NORTHWND; User ID = SA; Password = ***********";
+            return connect;
+        }
+    }
+}
---
+++ Lab.2/Lab.2/DataBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+
+namespace Lab._2
+{
+    public class DataBase
+    {
+        private IDbConnection _connection;
+        
+        public DataBase(string connectionStr)
+        {
+            _connection = new SqlConnection(connectionStr);
+        }
+
+        public IEnumerable<Employees> GetEmployees()
+        {
+            return _connection.Query<Employees>("SELECT*FROM Employees");
+        }
+        
+        public Employees GetEmployeesByID(int id)
+        {
+            return _connection.QuerySingleOrDefault<Employees>("SELECT*FROM Employees WHERE EmployeeID = @Id", new {Id = id});
+        }
+        
+        public bool AddEmployees(Employees employees)
+        {
+            var result = _connection.Execute("INSERT INTO Employees(LastName, FirstName) VALUES (@lastName, @firstName)", 
+                new {lastName = employees.LastName, firstName = employees.FirstName});
+            return result == 1;
+        }
+        
+        public Employees DropEmployees(int id)
+        {
+            var result = _connection.QuerySingleOrDefault<Employees>("DELETE Employees WHERE EmployeeID = @Id", new { Id = id });
+            _connection.Execute($"DBCC CHECKIDENT ( 'Employees', reseed , {id - 1})"); // reset auto increment
+            return result;
+        }
+        
+        public Employees UpdateEmpoyee(int id, string employeeName)
+        {
+
+            return _connection.QuerySingleOrDefault<Employees>("UPDATE Employees SET FirstName = @firstName WHERE EmployeeID = @Id", new { Id = id, firstName = employeeName });
+
+        }
+    }
+}
